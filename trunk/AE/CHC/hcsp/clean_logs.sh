757580df77f133b935e5845dc0e762ae7cee0de6
santiago.iturriaga <santiago.iturriaga@2f77c51a-9e79-da1f-09b5-e1d637586647>
2010-09-29 18:16:33 +0000

    20 iteraciones por config, con prioridades

--- trunk/AE/CHC/hcsp/clean_logs.sh
+++ trunk/AE/CHC/hcsp/clean_logs.sh
@@ -3,6 +3,9 @@
 mkdir -p calibracion/seq
 mkdir -p calibracion/lan4
 mkdir -p calibracion/lan8
+mkdir -p calibracion/seq_cprio
+mkdir -p calibracion/lan4_cprio
+mkdir -p calibracion/lan8_cprio
 mkdir -p seq
 mkdir -p lan4
 mkdir -p lan8
@@ -10,6 +13,9 @@ mkdir -p lan8
 rm calibracion/seq/*
 rm calibracion/lan4/*
 rm calibracion/lan8/*
+rm calibracion/seq_cprio/*
+rm calibracion/lan4_cprio/*
+rm calibracion/lan8_cprio/*
 rm seq/*
 rm lan4/*
 rm lan8/*
