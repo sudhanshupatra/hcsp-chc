15f1c70e140b6bcb7acd623b6ec7249887ba43ee
santiago.iturriaga <santiago.iturriaga@2f77c51a-9e79-da1f-09b5-e1d637586647>
2010-10-14 00:30:35 +0000

    Se agrega 2048x64

--- trunk/AE/CHC/hcsp/test.sh
+++ trunk/AE/CHC/hcsp/test.sh
@@ -1,1 +1,1 @@
-time(./MainSeq chc_test.cfg ../../ProblemInstances/HCSP/Braun_et_al.CPrio/u_c_hilo.0 res/chc_test.sol > res/chc_test.log)
+time(./MainSeq chc_test.cfg ../../ProblemInstances/HCSP/2048x64.mod/A.u_c_hihi.mod res/chc_test.sol > res/chc_test.log)
