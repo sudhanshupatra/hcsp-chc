0acd57a3c0e7bdd00b2c0f00848add092d664dde
santiago.iturriaga <santiago.iturriaga@2f77c51a-9e79-da1f-09b5-e1d637586647>
2010-12-30 12:21:45 +0000

    Cambios en la entrega de AE

---
+++ tags/1.1/CHC/hcsp/generar_scripts_evaluacion.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+Poblacion="15"
+Cruzamiento="0.8"
+Mutacion="0.9"
+Pesos="1.0 1.0"
+
+Filename="scripts_evaluacion/chc.cfg"
+echo "20			// number of independent runs" > $Filename
+echo "100000			// number of generations" >> $Filename
+echo "$Poblacion			// number of individuals" >> $Filename
+echo "0				// display state ?" >> $Filename
+echo "0				// seed, 0 = aleatorio" >> $Filename
+echo "$Pesos				// peso makespan, peso wqt" >> $Filename
+echo "Selection-Parameters	// selections to apply" >> $Filename
+echo "0.9 1 $Mutacion			// selection parameter, diverge operator & its probability" >> $Filename
+echo "Intra-Operators		// operators to apply in the population" >> $Filename
+echo "0 $Cruzamiento			// crossover & its probability" >> $Filename
+echo "Inter-Operators  		// operators to apply between this population and anothers" >> $Filename
+echo "0 50 5 1 3 1 5		// operator number, operator rate, number of individuals, selection of individual to send and remplace" >> $Filename
+echo "LAN-configuration" >> $Filename
+echo "1001			// refresh global state" >> $Filename
+echo "0				// 0: running in asynchronized mode / 1: running in synchronized mode" >> $Filename
+echo "1				// interval of generations to check solutions from other populations" >> $Filename
+	
---
+++ tags/1.1/CHC/hcsp/torque_seq_calibracion_cprio.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Nombre del trabajo
+#PBS -N ae_seq_cprio_cal
+
+# Requerimientos
+#PBS -l nodes=1:cpu8,walltime=250:00:00
+
+# Cola
+#PBS -q publica
+
+# Working dir
+#PBS -d /home/siturria/AE/trunk/AE/CHC/hcsp/
+
+# Correo electronico
+#PBS -M siturria@fing.edu.uy
+
+# Email
+#PBS -m abe
+# n: no mail will be sent.
+# a: mail is sent when the job is aborted by the batch system.
+# b: mail is sent when the job begins execution.
+# e: mail is sent when the job terminates.
+
+# Output path
+#PBS -e /home/siturria/AE/trunk/AE/CHC/hcsp/calibracion/seq_cprio/
+#PBS -o /home/siturria/AE/trunk/AE/CHC/hcsp/calibracion/seq_cprio/
+
+#PBS -V
+
+echo Job Name: $PBS_JOBNAME
+echo Working directory: $PBS_O_WORKDIR
+echo Queue: $PBS_QUEUE
+echo Cantidad de tasks: $PBS_TASKNUM
+echo Home: $PBS_O_HOME
+echo Puerto del MOM: $PBS_MOMPORT
+echo Nombre del usuario: $PBS_O_LOGNAME
+echo Idioma: $PBS_O_LANG
+echo Cookie: $PBS_JOBCOOKIE
+echo Offset de numero de nodos: $PBS_NODENUM
+echo Shell: $PBS_O_SHELL
+#echo JobID: $PBS_O_JOBID
+echo Host: $PBS_O_HOST
+echo Cola de ejecucion: $PBS_QUEUE
+echo Archivo de nodos: $PBS_NODEFILE
+echo Path: $PBS_O_PATH
+
+echo
+cd $PBS_O_WORKDIR
+echo Current path: 
+pwd
+echo
+echo Nodos:
+cat $PBS_NODEFILE
+echo
+# Define number of processors
+echo Cantidad de nodos:
+NPROCS=`wc -l < $PBS_NODEFILE`
+echo $NPROCS
+echo
+
+data[0]="u_c_hilo.0"
+data[1]="u_c_lohi.0"
+data[2]="u_s_hilo.0"
+data[3]="u_s_lohi.0"
+data[4]="u_i_hilo.0"
+data[5]="u_i_lohi.0"
+
+Poblacion[0]=5
+Poblacion[1]=10
+Poblacion[2]=15
+Poblacion[3]=20
+Poblacion[4]=30
+
+Cruzamiento[0]=0.7
+Cruzamiento[1]=0.8
+Cruzamiento[2]=0.9
+Cruzamiento[3]=1.0
+
+Mutacion[0]=0.5
+Mutacion[1]=0.7
+Mutacion[2]=0.9
+Mutacion[3]=1.0
+
+for indexP in {0..4}
+do
+	for indexC in {0..3}
+	do
+		for indexM in {0..3}
+		do
+			echo "Población ${Poblacion[indexP]}"
+			echo "Cruzamiento ${Cruzamiento[indexC]}"
+			echo "Mutación ${Mutacion[indexM]}"
+
+			for i in {0..5}
+			do
+				CfgFile="scripts_calibracion/chc_${Poblacion[indexP]}_${Cruzamiento[indexC]}_${Mutacion[indexM]}.cfg"
+				DataFile="../../ProblemInstances/HCSP/Braun_et_al.CPrio/${data[i]}"
+				OutputFile="calibracion/seq_cprio/${data[i]}_$CfgFile"
+				
+				echo "Datos $DataFile"
+				
+				time(./MainSeq $CfgFile $DataFile $OutputFile.sol > $OutputFile.log)    
+			done
+		done
+	done
+done
