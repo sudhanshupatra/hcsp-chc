c7ef43a93f830bf8be6ee7da310ad7ac6156028a
santiago.iturriaga <santiago.iturriaga@2f77c51a-9e79-da1f-09b5-e1d637586647>
2012-02-19 20:05:10 +0000

    Migracion a Makespan-Energy

--- CHC/hcsp/MainLan.cc
+++ CHC/hcsp/MainLan.cc
@@ -9,154 +9,200 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	using skeleton CHC;
-	char path[MAX_BUFFER] = "";
 
-	ifstream f(argv[1]);
-	if(!f) show_message(10);
+	string skeleton_file;
+	string scenario_file;
+	string workload_file;
+	string solution_file;
+	string pesos_file;
+	string cantidad_tareas;
+	string cantidad_maquinas;
 
-	// Leo desde el configuration file ================================
-	f.getline(path,MAX_BUFFER,'\n');
-	string skeleton_file(path);
-	ifstream f1(path);
-	if(!f1) show_message(11);
+	if (argc > 1) {
+		// ==================================================================
+		// Leo desde el configuration file
+		char path[MAX_BUFFER] = "";
 
-	f.getline(path,MAX_BUFFER,'\n');
-	string instance_file(path);
-	ifstream f2(path);
-	if(!f2) show_message(12);
+		ifstream f(argv[1]); // Configuration file.
+		if(!f) show_message(10);
 
-	f.getline(path,MAX_BUFFER,'\n'); // sol.txt
-	string solution_file(path);
+		f.getline(path,MAX_BUFFER,'\n'); // Archivo de configuración del skeleton.
+		skeleton_file.assign(path);
 
-	f.getline(path,MAX_BUFFER,'\n'); // pesos.txt
-	string pesos_file(path);
-	ifstream f3(path);
-	if(!f3) show_message(-1);
+		f.getline(path,MAX_BUFFER,'\n'); // Archivo con el scenario del problema a resolver.
+		scenario_file.assign(path);
 
+		f.getline(path,MAX_BUFFER,'\n'); // Archivo con el workload del problema a resolver.
+		workload_file.assign(path);
+
+		f.getline(path,MAX_BUFFER,'\n'); // Archivo de salida.
+		solution_file.assign(path);
+
+		f.getline(path,MAX_BUFFER,'\n'); // Archivo con pesos para las islas.
+		pesos_file.assign(path);
+
+		f.getline(path,MAX_BUFFER,'\n'); // Cantidad de tareas.
+		cantidad_tareas.assign(path);
+
+		f.getline(path,MAX_BUFFER,'\n'); // Cantidad de máquinas.
+		cantidad_maquinas.assign(path);
+	} else {
+		/*skeleton_file.assign("/home/santiago/eclipse/c-c++-workspace/AE/CHC/hcsp/ejecuciones/scripts_frente_pareto/barca.cfg");
+		scenario_file.assign("/home/santiago/eclipse/c-c++-workspace/AE/ProblemInstances/HCSP-3O-MPE/512x16/scenario.0");
+		workload_file.assign("/home/santiago/eclipse/c-c++-workspace/AE/ProblemInstances/HCSP-3O-MPE/512x16/workload.0");
+		solution_file.assign("test.sol");
+		pesos_file.assign("/home/santiago/eclipse/c-c++-workspace/AE/CHC/hcsp/ejecuciones/pesos_fijos.txt");
+		cantidad_tareas.assign("512");
+		cantidad_maquinas.assign("16");*/
+		cout << "[ERROR] Cantidad inválida de parámetros." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	cout << "[skeleton] " << skeleton_file << endl;
+	cout << "[scenario] " << scenario_file << endl;
+	cout << "[workload] " << workload_file << endl;
+	cout << "[priorities] " << priorities_file << endl;
+	cout << "[pesos] " << pesos_file << endl;
+
+	ifstream skeleton_stream(skeleton_file.data());
+	if(!skeleton_stream) show_message(11);
+
+	ifstream scenario_stream(scenario_file.data());
+	if(!scenario_stream) show_message(12);
+
+	ifstream workload_stream(workload_file.data());
+	if(!workload_stream) show_message(12);
+
+	// ==================================================================
+	// Inicializo el problema y el skeleton.
 	Problem pbm;
-	f2 >> pbm;
+
+	// Seteo el tamaño del problema.
+	pbm.setTaskCount(atoi(cantidad_tareas.data()));
+	pbm.setMachineCount(atoi(cantidad_maquinas.data()));
+
+	// Cargo la instancia a resolver.
+	// instance_stream >> pbm;
+	pbm.loadProblemDataFiles(scenario_stream, workload_stream);
 
 	Operator_Pool pool(pbm);
 	SetUpParams cfg(pool, pbm);
-	f1 >> cfg;
+	skeleton_stream >> cfg;
 
-	// ============================================
+	// ==================================================================
+	// Cargo los pesos de las islas.
 	vector<double> pesos;
-	string linea_pesos;
+	{
+		string linea_pesos;
 
-	f3 >> linea_pesos;
-	while (f3.good()) {
-		if (linea_pesos.length() > 0) {
-			//cout << linea_pesos << endl;
-			pesos.push_back(atof(linea_pesos.data()));
+		ifstream pesos_stream(pesos_file.data());
+		if(!pesos_stream) show_message(-1);
+
+		if (pesos_stream.is_open()) {
+			while (!pesos_stream.eof()) {
+				pesos_stream >> linea_pesos;
+
+				if (linea_pesos.length() > 0) {
+					pesos.push_back(atof(linea_pesos.data()));
+				}
+			}
 		}
-		f3 >> linea_pesos;
+
+		pesos_stream.close();
+
+		pbm.loadWeightData(pesos);
 	}
-	f3.close();
 
-	pbm.loadWeights(pesos);
-	// ============================================
-
+	// ==================================================================
+	// Inicio la ejecución del algoritmo.
 	Solver_Lan solver(pbm,cfg,argc,argv);
 	solver.run();
 
+	// ==================================================================
+	// Terminó el algoritmo. Escribo los resultado a disco.
 	if (solver.pid()!=0)
 	{
+		// Si es una isla de cómputo...
+
 		char str_pid[100];
 		sprintf(str_pid, "%d", solver.pid());
+		solution_file = solution_file.append("_").append(str_pid);
 
 		{
-			solution_file = solution_file.append("_").append(str_pid);
-			ofstream fexit(solution_file.data());
+			// Escribo el la evaluación de las métricas en cada individuo de la población (padre e hijos).
+			string makespan_solution_file = solution_file.append("_metricas");
+			ofstream fexit(makespan_solution_file.data());
 			if(!fexit) show_message(13);
 
-			fexit << solver.best_solution_trial().makespan() << " " << solver.best_solution_trial().accumulatedWeightedResponseRatio() << " " << solver.pid() << endl;
+			fexit << solver.best_solution_trial().getMakespan()
+					<< " " << solver.best_solution_trial().getEnergy(solver.best_solution_trial().getMakespan())
+					<< " " << solver.pid() << endl;
 
 			for (int i = 0; i < solver.population().parents().size(); i++) {
-				fexit << solver.population().parents()[i]->makespan() << " " << solver.population().parents()[i]->accumulatedWeightedResponseRatio() << " " << solver.pid() << endl;
+				fexit << solver.population().parents()[i]->getMakespan()
+						<< " " << solver.population().parents()[i]->getEnergy(solver.population().parents()[i]->getMakespan())
+						<< " " << solver.pid() << endl;
 			}
 
 			for (int i = 0; i < solver.population().offsprings().size(); i++) {
-				fexit << solver.population().offsprings()[i]->makespan() << " " << solver.population().offsprings()[i]->accumulatedWeightedResponseRatio() << " " << solver.pid() << endl;
+				fexit << solver.population().offsprings()[i]->getMakespan()
+						<< " " << solver.population().parents()[i]->getEnergy(solver.population().offsprings()[i]->getMakespan())
+						<< " " << solver.pid() << endl;
 			}
 
 			fexit.close();
 		}
 		{
-			//string fit_solution_file = solution_file.append("_fit");
-			//ofstream fexit(fit_solution_file.data());
-			//if(!fexit) show_message(13);
+			// Escribo el fitness de cada individuo de la población (padres e hijos).
+			string fit_solution_file = solution_file.append("_fit");
+			ofstream fexit(fit_solution_file.data());
+			if(!fexit) show_message(13);
 
-			//fexit << solver.best_solution_trial().fitness() << " " << solver.pid() << endl;
-			//solver.best_solution_trial().show(fexit);
+			fexit << solver.best_solution_trial().getFitness() << " " << solver.pid() << endl;
 
-			//for (int i = 0; i < solver.population().parents().size(); i++) {
-				//fexit << solver.population().parents()[i]->fitness() << " " << solver.pid() << endl;
-				//fexit << "-2" << endl;
-				//solver.population().parents()[i]->show(fexit);
-			//}
+			for (int i = 0; i < solver.population().parents().size(); i++) {
+				fexit << solver.population().parents()[i]->getFitness() << " " << solver.pid() << endl;
+			}
 
-			//for (int i = 0; i < solver.population().offsprings().size(); i++) {
-				//fexit << solver.population().offsprings()[i]->fitness() << " " << solver.pid() << endl;
-				//fexit << "-2" << endl;
-				//solver.population().offsprings()[i]->show(fexit);
-			//}
+			for (int i = 0; i < solver.population().offsprings().size(); i++) {
+				fexit << solver.population().offsprings()[i]->getFitness() << " " << solver.pid() << endl;
+			}
 
-			//fexit.close();
+			fexit.close();
 		}
 
 		{
+			// Escribo algo de meta información de la isla (p.ej.: que pesos le fueron asignados).
 			string meta_solution_file = solution_file.append("_meta");
 			ofstream fexit(meta_solution_file.data());
 			if(!fexit) show_message(13);
 
 			fexit << "Pesos asignados" << endl;
 			fexit << "Makespan weight: " << pbm.getMakespanWeight(solver.pid()) << endl;
-			fexit << "WRR weight: " << pbm.getWRRWeight(solver.pid()) << endl;
 
 			fexit.close();
 		}
 	}
 	else {
+		// Si es la isla master...
 		cout << "[INFO] Exec: " << argv[0] << endl;
 		cout << "[INFO] Configuration file: " << argv[1] << endl;
 		cout << "[CONFIG] Skeleton file: " << skeleton_file << endl;
-		cout << "[CONFIG] Instancia: " << instance_file << endl;
+		cout << "[CONFIG] Scenario: " << scenario_file << endl;
+		cout << "[CONFIG] Workload: " << workload_file << endl;
 		cout << "[CONFIG] Summary:" << solution_file << endl;
-		cout << "[CONFIG] Pesos:" << pesos_file << endl << endl;
-
-		cout << "[CONFIG] Pesos: " << pesos.size() << endl;
-		for (unsigned int i = 0; i < pesos.size() - 1; i = i + 2) {
-			cout << "(Makespan: " << pesos[i] << ", WRR: " << pesos[i+1] << ")" << endl;
-		}
+		cout << "[CONFIG] Archivo de pesos:" << pesos_file << endl << endl;
 		assert(pesos.size() % 2 == 0);
+		cout << "[CONFIG] Pesos cargados: " << pesos.size() / 2 << endl;
+		for (unsigned int i = 0; i < pesos.size() - 1; i = i + 2) {
+			cout << "(Makespan: " << pesos[i] << ", Energy: " << pesos[i+1] << ")" << endl;
+		}
 		cout << endl << endl;
 
 		cout << cfg;
 
 		solver.statistics();
 		solver.show_state();
-
-		cout << "Makespan: " << solver.global_best_solution().makespan() << endl;
-		cout << "WRR: " << solver.global_best_solution().accumulatedWeightedResponseRatio() << endl;
-		cout << "Makespan (reference): " << Solution::getMakespan_reference() << endl;
-		cout << "WRR (reference): " << Solution::getWRR_reference() << endl;
-
-		double weight_mks = 0.0, weight_wrr = 0.0;
-		double min_fitness = INFINITY;
-		for (unsigned int i = 0; i < pesos.size() - 1; i=i+2) {
-			double aux_fitness;
-			aux_fitness = pesos[i]*(Solution::getMakespan_reference()+solver.global_best_solution().makespan())/Solution::getMakespan_reference()
-			+ pesos[i+1]*(Solution::getWRR_reference()+solver.global_best_solution().accumulatedWeightedResponseRatio())/Solution::getWRR_reference();
-
-			if (aux_fitness < min_fitness) {
-				min_fitness = aux_fitness;
-				weight_mks = pesos[i];
-				weight_wrr = pesos[i+1];
-			}
-		}
-
-		cout << "Fitness: " << min_fitness << " (" << weight_mks << ", " << weight_wrr << ")" << endl;
 
 		ofstream fexit(solution_file.data());
 		if(!fexit) show_message(13);
