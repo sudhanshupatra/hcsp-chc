f2cba2db0fa7a5ad008da1babde85ff7f501ac0e
santiago.iturriaga <santiago.iturriaga@2f77c51a-9e79-da1f-09b5-e1d637586647>
2012-02-24 00:15:22 +0000

    Mejor FP

--- CHC/hcsp/tests/prueba_dora.sh
+++ CHC/hcsp/tests/prueba_dora.sh
@@ -1,18 +1,34 @@
 working_path="/home/santiago/workspace/AE-MF-Barca-2011/CHC/hcsp"
 scripts_path="/home/santiago/Scheduling/Instances/Makespan/512x16"
-instance="B.u_c_hihi"
+instance_i="B.u_i_hihi"
+instance_c="B.u_c_hihi"
 
-CfgFile="${working_path}/ejecuciones/scripts_frente_pareto/barca2.cfg"
-DataFile="${scripts_path}/${instance}"
-OutputFile="${working_path}/tests/resultados/prueba_lan_${data[i]}"
-PesosFile="${working_path}/ejecuciones/pesos_fijos.txt"
+DataFileI="${scripts_path}/${instance_i}"
+OutputFileI="${working_path}/tests/resultados/i_${data[i]}"
+
+DataFileC="${scripts_path}/${instance_c}"
+OutputFileC="${working_path}/tests/resultados/c_${data[i]}"
+
+CfgFile="${working_path}/ejecuciones/config-0512.cfg"
+PesosFile="${working_path}/ejecuciones/pesos_8.txt"
 
 echo "${CfgFile}" > ${working_path}/tests/resultados/prueba_lan.cfg
-echo "$DataFile" >> ${working_path}/tests/resultados/prueba_lan.cfg
-echo "$OutputFile.sol" >> ${working_path}/tests/resultados/prueba_lan.cfg
-echo "$PesosFile" >> ${working_path}/tests/resultados/prueba_lan.cfg
+echo "${DataFileI}" >> ${working_path}/tests/resultados/prueba_lan.cfg
+echo "${OutputFileI}.sol" >> ${working_path}/tests/resultados/prueba_lan.cfg
+echo "${PesosFile}" >> ${working_path}/tests/resultados/prueba_lan.cfg
 echo "512" >> ${working_path}/tests/resultados/prueba_lan.cfg
 echo "16" >> ${working_path}/tests/resultados/prueba_lan.cfg
 
-time(mpirun -n 3 ${working_path}/MainLan \
-	${working_path}/tests/resultados/prueba_lan.cfg > $OutputFile.log) 
+time(mpirun -n 8 ${working_path}/MainLan \
+	${working_path}/tests/resultados/prueba_lan.cfg > ${OutputFileI}.log) 
+
+echo "${CfgFile}" > ${working_path}/tests/resultados/prueba_lan.cfg
+echo "${DataFileC}" >> ${working_path}/tests/resultados/prueba_lan.cfg
+echo "${OutputFileC}.sol" >> ${working_path}/tests/resultados/prueba_lan.cfg
+echo "${PesosFile}" >> ${working_path}/tests/resultados/prueba_lan.cfg
+echo "512" >> ${working_path}/tests/resultados/prueba_lan.cfg
+echo "16" >> ${working_path}/tests/resultados/prueba_lan.cfg
+
+time(mpirun -n 8 ${working_path}/MainLan \
+    ${working_path}/tests/resultados/prueba_lan.cfg > ${OutputFileC}.log) 
+	
--- CHC/hcsp/tests/resultados/metricas.sh
+++ CHC/hcsp/tests/resultados/metricas.sh
@@ -1,3 +1,9 @@
-cat *.sol_? > metricas.log
-cat *.sol_?? >> metricas.log
-./FP_2obj_3col metricas.log
+cat i_.sol_? > metricas_i.log
+cat i_.sol_?? >> metricas_i.log
+./FP_2obj_3col metricas_i.log
+mv FP.out FP_i.out
+
+cat c_.sol_? > metricas_c.log
+cat c_.sol_?? >> metricas_c.log
+./FP_2obj_3col metricas_c.log
+mv FP.out FP_c.out
